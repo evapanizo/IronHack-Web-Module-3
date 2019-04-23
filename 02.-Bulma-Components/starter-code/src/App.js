08c1182de9d33c85eb8e69b38a65601541e96b1a
Eva Panizo <panizo.eva@gmail.com>
2019-04-24 00:02:49 +0200

    02.- Bulma Components - Create basic files/folders + install & import bulma

---
+++ 02.-Bulma-Components/starter-code/src/App.js
@@ -0,0 +1,16 @@
+// Module dependencies
+import React, { Component } from 'react';
+
+// App
+class App extends Component {
+
+  render() {
+    return (
+      <main>
+      </main>
+    )
+  }
+}
+
+// Export
+export default App;
