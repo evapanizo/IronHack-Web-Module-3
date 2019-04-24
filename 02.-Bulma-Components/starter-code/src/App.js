5fac746397e633b92c944122a2f164b16ba46a11
Eva Panizo <panizo.eva@gmail.com>
2019-04-24 21:52:10 +0200

    02.- Bulma Components - Iteration 1, 2 & 3 completed

--- 02.-Bulma-Components/starter-code/src/App.js
+++ 02.-Bulma-Components/starter-code/src/App.js
@@ -1,12 +1,19 @@
 // Module dependencies
 import React, { Component } from 'react';
 
+// Project dependencies
+/// Components
+import Navbar from './components/Navbar';
+import FormField from './components/FormField';
+
 // App
 class App extends Component {
-
   render() {
     return (
       <main>
+        <Navbar/>
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
       </main>
     )
   }
