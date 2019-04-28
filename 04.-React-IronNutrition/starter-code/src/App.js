7f4ffaa42375307cbfbbf88d88de630883c22d2d
Eva Panizo <panizo.eva@gmail.com>
2019-04-29 01:03:23 +0200

    04.- IronNutrition - Iteration 4 completed

--- 04.-React-IronNutrition/starter-code/src/App.js
+++ 04.-React-IronNutrition/starter-code/src/App.js
@@ -6,13 +6,15 @@ import listOfFoods from './data/foods.json';
 /// Components
 import FoodBox from './components/FoodBox';
 import FoodForm from './components/FoodForm';
+import Search from './components/Search';
 
 // App
 class App extends Component {
 
   state = {
     foods: listOfFoods,
-    isForm: false
+    isForm: false,
+    searchValue: ''
   }
 
   handleClickAdd = () => {
@@ -30,15 +32,29 @@ class App extends Component {
     })
   }
 
+  handleSearch = searchValue => {
+    this.setState({
+      searchValue: searchValue,
+    })
+  }
+
+  renderFoods = (foodArray, searchValue) => {
+    const filteredArray = foodArray.filter( food => {
+      return food.name.toLowerCase().includes(searchValue.toLowerCase())
+    })
+    return filteredArray.map((food, index) => <FoodBox key={index} food={food}/>)
+  }
+
   render() { 
-    const {handleClickAdd, handleAddNewFood} = this;
-    const {isForm, foods} = this.state;
+    const {handleClickAdd, handleAddNewFood, handleSearch, renderFoods} = this;
+    const {isForm, foods, searchValue} = this.state;
     return (
       <main>
         <h1 className="title is-2">IronNutrition</h1>
         <button className="btn is-info" onClick={handleClickAdd}>Add New Food</button>
         {isForm ? <FoodForm handleAddNewFood={handleAddNewFood}/> : null}
-        {foods.map((food, index) => <FoodBox key={index} food={food}/>)}
+        <Search handleSearch={handleSearch}/>
+        {renderFoods(foods, searchValue)}
       </main>
     )
   }
